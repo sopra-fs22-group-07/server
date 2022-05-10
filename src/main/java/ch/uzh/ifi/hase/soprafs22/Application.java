c2602642b5b0c35c35bd262b61d9acd6a07f8c93
anhuwi <andreas.huwiler@uzh.ch>
2022-05-10 23:21:22 +0200

    create test Repository

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -3,16 +3,13 @@ package ch.uzh.ifi.hase.soprafs22;
 import ch.uzh.ifi.hase.soprafs22.constant.Gender;
 import ch.uzh.ifi.hase.soprafs22.entity.User;
 import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,26 +21,13 @@ import java.util.Date;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
     // boot the app
     System.out.println("Starting application...");
     SpringApplication.run(Application.class, args);
   }
 
-  /*
-    @Bean
-    public CommandLineRunner demoData(UserRepository userRepository) {
-        return (args -> {
-            User user1 = new User();
-            user1.setName("Adam");
-            user1.setPassword("1234");
-            user1.setGender(Gender.MALE);
-            user1.setBirthday(new Date());
-            userRepository.save(user1);
-        });
-    }
-
-
+/*
   @Bean
   public CommandLineRunner run(UserRepository userRepository){
       return(args -> {
