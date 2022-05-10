a40f930ab77d424011f56bdbe7b3d78f1bd0cfb3
anhuwi <andreas.huwiler@uzh.ch>
2022-05-11 00:05:41 +0200

    created database file

--- src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/Application.java
@@ -1,10 +1,5 @@
 package ch.uzh.ifi.hase.soprafs22;
 
-import ch.uzh.ifi.hase.soprafs22.constant.Gender;
-import ch.uzh.ifi.hase.soprafs22.entity.User;
-import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Date;
 
 
 @RestController
@@ -26,26 +19,6 @@ public class Application {
     System.out.println("Starting application...");
     SpringApplication.run(Application.class, args);
   }
-
-/*
-  @Bean
-  public CommandLineRunner run(UserRepository userRepository){
-      return(args -> {
-          insertUser(userRepository);
-          System.out.println(userRepository.findAll());
-      });
-  }
-
-  private void insertUser(UserRepository userRepository){
-      // user1
-      User user1 = new User();
-      user1.setName("Adam");
-      user1.setPassword("1234");
-      user1.setGender(Gender.MALE);
-      user1.setBirthday(new Date());
-      userRepository.save(user1);
-
-  }*/
 
   @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
   @ResponseStatus(HttpStatus.OK)
