5e2977b2fcf0252ed615e8f1d8df233d34fb3ef4
joemu <joe.mueller@uzh.ch>
2022-05-17 11:44:06 +0200

    -White Cards and Black Cards now are more randomly distributed

--- src/main/java/ch/uzh/ifi/hase/soprafs22/repository/BlackCardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/repository/BlackCardRepository.java
@@ -2,10 +2,17 @@ package ch.uzh.ifi.hase.soprafs22.repository;
 
 import ch.uzh.ifi.hase.soprafs22.entity.BlackCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("BlackCardRepository")
 public interface BlackCardRepository extends JpaRepository<BlackCard, Long> {
   BlackCard findById(long id);
   BlackCard findByText(String text);
+
+
+  @Query("select blackCard.id from BlackCard blackCard")
+  List<Long> getAllIds();
 }
