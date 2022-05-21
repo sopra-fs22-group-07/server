0dd730236346cff2fc8e92f5468082771c471222
anhuwi <andreas.huwiler@uzh.ch>
2022-05-21 22:38:06 +0200

    websockets

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/WebSocketTextController.java
@@ -0,0 +1,36 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.rest.dto.chat.ChatMessagePutDTO;
+import ch.uzh.ifi.hase.soprafs22.rest.dto.chat.TextMessageDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class WebSocketTextController {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @PostMapping("/send")
+    public ResponseEntity<Void> sendMessage(@RequestBody TextMessageDTO textMessageDTO) {
+        template.convertAndSend("/topic/message", textMessageDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @MessageMapping("/sendMessage")
+    public void receiveMessage(@Payload TextMessageDTO textMessageDTO) {
+        // receive message from client
+    }
+
+
+    @SendTo("/topic/message")
+    public TextMessageDTO broadcastMessage(@Payload TextMessageDTO textMessageDTO) {
+        return textMessageDTO;
+    }
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/chat/TextMessageDTO.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs22.rest.dto.chat;
+
+public class TextMessageDTO {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
