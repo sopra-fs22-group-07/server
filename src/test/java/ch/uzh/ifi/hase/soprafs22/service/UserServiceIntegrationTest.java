a574d1f585a67717cad916696c7ca6c1ffdf99a1
Seraina <seraina.schraff@uzh.ch>
2022-05-21 15:30:54 +0200

    added to UserServiceIntegrationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +31,8 @@ import static org.junit.jupiter.api.Assertions.*;
         locations = "application-integrationtest.properties")
 class UserServiceIntegrationTest {
 
+    private User testUser;
+
   @Qualifier("userRepository")
   @Autowired
   private UserRepository userRepository;
@@ -42,30 +45,26 @@ class UserServiceIntegrationTest {
     @Autowired
     private MatchRepository matchRepository;
 
-
   @Autowired
   private UserService userService;
 
-
-
   @BeforeEach
   public void setup() {
+    testUser = new User();
+    testUser.setUsername("username");
+    testUser.setName("name");
+    testUser.setPassword("password");
+    testUser.setToken("1234");
+    testUser.setGender(Gender.FEMALE);
+    testUser.setBirthday(new Date());
+
     userRepository.deleteAll();
   }
 
   @Test
   void createUser_validInputs_success() {
     // given
-    assertNull(userRepository.findByUsername("testUsername"));
-
-    User testUser = new User();
-
-      testUser.setUsername("username");
-      testUser.setName("name");
-      testUser.setPassword("password");
-      testUser.setToken("1234");
-      testUser.setGender(Gender.FEMALE);
-      testUser.setBirthday(new Date());
+    assertNull(userRepository.findByUsername(testUser.getUsername()));
 
     // when
     User createdUser = userService.createUser(testUser);
@@ -74,22 +73,16 @@ class UserServiceIntegrationTest {
     assertEquals(testUser.getId(), createdUser.getId());
     assertEquals(testUser.getName(), createdUser.getName());
     assertEquals(testUser.getUsername(), createdUser.getUsername());
+    assertEquals(18, createdUser.getMinAge());
+    assertEquals(3, createdUser.getGenderPreferences().size());
+    assertTrue(createdUser.getGenderPreferences().contains(Gender.MALE));
     assertNotNull(createdUser.getToken());
     assertEquals(UserStatus.OFFLINE, createdUser.getStatus());
   }
 
   @Test
   void createUser_duplicateUsername_throwsException() {
-    assertNull(userRepository.findByUsername("testUsername"));
-
-    User testUser = new User();
-    testUser.setName("testName");
-    testUser.setUsername("testUsername");
-    testUser.setStatus(UserStatus.ONLINE);
-    testUser.setToken("a");
-    testUser.setPassword("1234");
-    testUser.setBirthday(new Date());
-
+    assertNull(userRepository.findByUsername(testUser.getUsername()));
 
     User createdUser = userService.createUser(testUser);
 
@@ -98,7 +91,7 @@ class UserServiceIntegrationTest {
 
     // change the name but forget about the username
     testUser2.setName("testName2");
-    testUser2.setUsername("testUsername");
+    testUser2.setUsername(testUser.getUsername());
     testUser2.setStatus(UserStatus.ONLINE);
     testUser2.setToken("b");
     testUser2.setPassword("1234");
@@ -106,5 +99,39 @@ class UserServiceIntegrationTest {
 
     // check that an error is thrown
     assertThrows(ResponseStatusException.class, () -> userService.createUser(testUser2));
+  }
+
+  @Test
+  void getAllUsers_success() {
+      User testUser2 = new User();
+      testUser2.setUsername("username2");
+      testUser2.setName("name2");
+      testUser2.setPassword("password2");
+      testUser2.setToken("12345");
+      testUser2.setGender(Gender.FEMALE);
+      testUser2.setBirthday(new Date());
+
+      // given
+      assertNull(userRepository.findByUsername(testUser.getUsername()));
+      assertNull(userRepository.findByUsername(testUser2.getUsername()));
+
+      userService.createUser(testUser);
+      userService.createUser(testUser2);
+
+      List<User> res = userService.getUsers();
+      assertEquals(2, res.size());
+      assertEquals(res.get(0).getId(), testUser.getId());
+      assertEquals(res.get(1).getId(), testUser2.getId());
+  }
+
+  @Test
+  void deleteUser_success() {
+    assertNull(userRepository.findByUsername(testUser.getUsername()));
+
+    userService.createUser(testUser);
+    assertNotNull(userRepository.findByUsername(testUser.getUsername()));
+
+    userService.deleteUser(testUser.getId());
+    assertNull(userRepository.findByUsername(testUser.getUsername()));
   }
 }
