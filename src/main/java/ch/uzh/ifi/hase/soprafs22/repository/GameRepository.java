5b6c28114d9dc47a91b1bf1bdc6c29a48fad06d7
david <david.alexander@gmx.ch>
2022-05-26 17:26:37 +0200

    Remove @Params from count to see what happens

--- src/main/java/ch/uzh/ifi/hase/soprafs22/repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/repository/GameRepository.java
@@ -34,18 +34,18 @@ public interface GameRepository extends JpaRepository<Game, Long> {
                                                             @Param("matched") Set<User> matched);
 
     @Query(value = "select count (game) from Game game where game.gameStatus = ch.uzh.ifi.hase.soprafs22.constant.GameStatus.ACTIVE " +
-            "and game.user <> :user " +
-            "and game not in (select g from Game g join Play p on g.id = p.gameId where p.userId = :userId)" +
-            "and game in (select g from Game g, User u where u = g.user and u not in :blocked and u not in :matched)"+
-            "and game in (select g from Game g join User u on g.user=u where u.birthday between :maxAgeDate and :minAgeDate)" +
-            "and game in(select g from Game g, User u, User u2 where u = g.user and :gender member of u.genderPreferences and u.gender member of u2.genderPreferences and u2.id = :userId)")
-    Long countOtherUserWithActiveGameThatWasNotPlayedOn(@Param("userId") long userId,
-                                                        @Param("user") User user,
-                                                        @Param("gender") Gender gender,
-                                                        @Param("minAgeDate") Date minAgeDate,
-                                                        @Param("maxAgeDate") Date maxAgeDate,
-                                                        @Param("blocked") Set<User> blocked,
-                                                        @Param("matched") Set<User> matched);
+            "and game.user <> ?2 " +
+            "and game not in (select g from Game g join Play p on g.id = p.gameId where p.userId = ?1)" +
+            "and game in (select g from Game g, User u where u = g.user and u not in ?6 and u not in ?7)"+
+            "and game in (select g from Game g join User u on g.user=u where u.birthday between ?5 and ?4)" +
+            "and game in(select g from Game g, User u, User u2 where u = g.user and ?3 member of u.genderPreferences and u.gender member of u2.genderPreferences and u2.id = ?1)")
+    Long countOtherUserWithActiveGameThatWasNotPlayedOn(long userId,
+                                                        User user,
+                                                        Gender gender,
+                                                        Date minAgeDate,
+                                                        Date maxAgeDate,
+                                                        Set<User> blocked,
+                                                        Set<User> matched);
 
 }
 
